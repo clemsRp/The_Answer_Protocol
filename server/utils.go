4e5a7133d0911cf87e8453909daa64e58603b7f6
Clement Rappo <crappo@z3r12p1.42lyon.fr>
2026-07-06 10:41:14 +0200

    Update because of error

--- server/utils.go
+++ server/utils.go
@@ -2,7 +2,7 @@ package main
 
 func inform_room(clients map[string]*Client, cli *Client, room string, msg string) {
 	for ip := range clients {
-		if clients[ip].datas.room == room && clients[ip].name != cli.name {
+		if clients[ip].datas.room == room && clients[ip].name != cli.name && clients[ip].datas.connected {
 			clients[ip].ch <- Response{msg, Datas{}, Request{}}
 		}
 	}
