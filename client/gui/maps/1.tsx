b99fe4c1802ba70cc401aeaf3578c0283c38bb51
Ali Uslu <aluslu@z1r2p5.42lyon.fr>
2026-09-03 15:00:01 +0200

    added 7 maps with Tiled

---
+++ client/gui/maps/1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="1" tilewidth="48" tileheight="48" tilecount="256" columns="16">
+ <image source="../The_Answer_Protocol/client/gui/assets/1.png" width="768" height="768"/>
+</tileset>
